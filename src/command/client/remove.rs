8a46a42eb55852169958e0b9e1f93392cd940f32
Wayan-Gwie Lapointe <wayangwie@gmail.com>
2023-10-01 23:53:43 -0400

    Guess alternative command name when no command is found in update or delete

--- src/command/client/remove.rs
+++ src/command/client/remove.rs
@@ -13,15 +13,19 @@ impl Cmd {
     pub fn run(self) -> AppResult<()> {
         let engine = Database::init()?;
 
-        let name = loop {
-            let name: String = prompt("Enter a name for the command in Chest")?;
-            if engine.get_command(&name).is_some() {
-                break name;
+        let name: String = prompt("Enter a name for the command in Chest")?;
+        if engine.get_command(&name).is_some() {
+            engine.remove_command(&name)
+        } else {
+            if let Some(alt_command) = engine.search_commands_strict(&name) {
+                println!(
+                    "No command by this name. Did you mean {}?",
+                    alt_command.name
+                );
             } else {
                 println!("No command by this name.");
             }
-        };
-
-        engine.remove_command(&name)
+            Ok(())
+        }
     }
 }
--- src/command/client/update.rs
+++ src/command/client/update.rs
@@ -13,25 +13,29 @@ impl Cmd {
     pub fn run(self) -> AppResult<()> {
         let engine = Database::init()?;
 
-        let mut command = loop {
-            let name: String = prompt("Enter a name for the command in Chest")?;
-            if let Some(command) = engine.get_command(&name) {
-                break command;
+        let name: String = prompt("Enter a name for the command in Chest")?;
+        if let Some(mut command) = engine.get_command(&name) {
+            if let Some(command_text) =
+                prompt_opt("Enter the new command text (empty to keep current)")?
+            {
+                command.command_text = command_text;
+            }
+
+            if let Some(description) = prompt_opt("Enter the new description (empty to keep)")? {
+                command.description = description;
+            }
+
+            engine.update_command(command)
+        } else {
+            if let Some(alt_command) = engine.search_commands_strict(&name) {
+                println!(
+                    "No command by this name. Did you mean {}?",
+                    alt_command.name
+                );
             } else {
                 println!("No command by this name.");
             }
-        };
-
-        if let Some(command_text) =
-            prompt_opt("Enter the new command text (empty to keep current)")?
-        {
-            command.command_text = command_text;
+            Ok(())
         }
-
-        if let Some(description) = prompt_opt("Enter the new description (empty to keep)")? {
-            command.description = description;
-        }
-
-        engine.update_command(command)
     }
 }
